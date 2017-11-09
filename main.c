f4d4c7f3119f764849cb04be14704ab013803a92
Joshua E Bearden <josh.e.bearden@gmail.com>
2017-11-09 15:02:39 -0600

    initial complete of main.c

--- main.c
+++ main.c
@@ -8,18 +8,25 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <isnum.h>
+#include <string.h>
 
 #define MAX 200 //set max amount of arguments
 
 int main(int argc, char** argv)
 {
+   int total = 0;
    int i;
+   int j;
    if(argc > MAX)
    {
       printf("Too many arguments! The most you can have is %d.\n", MAX);
       return 1;
    }
-   else if()
+   else if(argc == 1)
+   {
+      printf("No arguments!\n");
+      return 2;
+   }
 
    //main loop
    //go through the command line args
@@ -27,6 +34,19 @@ int main(int argc, char** argv)
    //check for substrings if string
    for(i = 1; i < argc; i++)
    {
-
+      if(isnum(argv[i]))
+      {
+         total += atoi(argv[i]);
+      }
+      else
+      {
+         for(j = i+1; j < argc; j++)
+         {
+            if(strstr(argv[j], argv[i]))
+            {
+               printf("%s is a substring of %s\n", argv[i], argv[j]);
+            }
+         }
+      }
    }
 }
