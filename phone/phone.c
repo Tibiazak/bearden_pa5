052d802a0505bcbebb0918b9eea3af583baf7ed7
Joshua E Bearden <josh.e.bearden@gmail.com>
2017-11-14 13:23:04 -0600

    forgot an ampersand

--- phone/phone.c
+++ phone/phone.c
@@ -9,7 +9,7 @@
 int main()
 {
    char num[10];
-   char * number = num[0];
+   char * number = &num[0];
    printf("Please enter a phone number in any format: ");
    number = phone_fmt(&num[0]);
    if(number != NULL)
