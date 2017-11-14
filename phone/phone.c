47714b232b4003bd5838889d6195dfe497e7e2bd
Joshua E Bearden <josh.e.bearden@gmail.com>
2017-11-14 13:57:36 -0600

    added comments

--- phone/phone.c
+++ phone/phone.c
@@ -7,16 +7,23 @@
 #include <stdio.h>
 #include "phone_fmt.h"
 
+//A program that gets input containing up to 200 characters
+//and formats the first 10 digits as a phone number.
+
 int main()
 {
-   int i = 0;
-   char num[11];
+   //variable declaration
+   char num[11]; //array for the phone number
    char * number = &num[0];
-   num[10] = '\0';
+   //num[10] = '\0';
+
    printf("Please enter a phone number in any format: ");
-   number = phone_fmt(&num[0]);
+   number = phone_fmt(&num[0]); //calls the function phone_fmt to get input and select the digits
+
+   //phone_fmt() returns a null pointer if it fails to find 10 digits
    if(number != NULL)
    {
+      //if phone_fmt was successful, format output as a phone number and print
       printf("(%c%c%c) %c%c%c-%c%c%c%c\n", num[0],num[1],num[2],num[3],num[4],num[5],num[6],num[7],num[8],num[9]);
       return 0;
    }
--- phone/phone_fmt.c
+++ phone/phone_fmt.c
@@ -7,24 +7,32 @@
 #include <stdio.h>
 #include <ctype.h>
 
+//a function that gets from input up to 200 characters and finds the first 10 digits
 char * phone_fmt(char * num)
 {
+   //variable declaration
    int i = 0;
    int j = 0;
-   char input[200];
-   fgets(input, 200, stdin);
+   char input[200]; //input storage array
+
+   fgets(input, 200, stdin); //read input and store it
+
+   //check every item in the input array, if it is a digit then
+   //add it to the array passed to the function, otherwise skip it
+   //if the item in the array is null or a newline, fail
+   //if the end of the array is reached before 10 digits are, fail
    for(i = 0; i < 200; i++)
    {
-      if(j == 10)
+      if(j == 10) //check to see if we've seen 10 digits
       {
          return num;
       }
-      if(isdigit(input[i]))
+      if(isdigit(input[i])) //check if the current char is a digit
       {
          num[j] = input[i];
          j++;
       }
-      else if((input[i] == "\n") || (input[i] == NULL))
+      else if((input[i] == "\n") || (input[i] == NULL)) //check if we've exhausted input
       {
          return NULL;
       }
